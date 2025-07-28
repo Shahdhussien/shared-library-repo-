9436e68f65c305ba8fb79eff35b27ff001d5d7a0
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 06:09:28 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,18 +1,36 @@
+// def call() {
+//     return {
+//         stage('Push Manifests') {
+//             sshagent(['github']) {
+//                 sh '''
+//                     echo "hhhhhhhhhhh"
+//                     git config user.name "Jenkins"
+//                     git config user.email "jenkins@example.com"
+//                     git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+//                     git checkout main
+//                     git add .
+//                     git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+//                     git push origin main
+//                 '''
+//             }
+//         }
+//     }
+// }
+
 def call() {
-    return {
-        stage('Push Manifests') {
-            sshagent(['github']) {
-                sh '''
-                    echo "hhhhhhhhhhh"
-                    git config user.name "Jenkins"
-                    git config user.email "jenkins@example.com"
-                    git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
-                    git checkout main
-                    git add .
-                    git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
-                    git push origin main
-                '''
-            }
+    stage('Push Manifests') {
+        sshagent(['github']) {
+            sh '''
+             echo "hhhhhhhhhhh"
+                echo "🚀 Committing updated manifests..."
+                git config user.name "Jenkins"
+                git config user.email "jenkins@example.com"
+                git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+                git checkout main
+                git add .
+                git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+                git push origin main
+            '''
         }
     }
 }
