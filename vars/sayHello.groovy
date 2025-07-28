c2b6a659356d5e4bec8b945c7e27d3fb00c43541
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 05:44:02 +0300

    first commit

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,14 @@
-stage('sayHello') {
-  steps {
-    sh '''
-      pwd
-      ls -l
-      ls -l K8s || echo "No K8s folder"
-      cat K8s/deployment.yaml || echo "No deployment.yaml"
+
+
+def call() {
+    stage('DEBUG Check Files') {
+      steps {
+        sh '''
+          pwd
+          ls -l
+          ls -l K8s || echo "No K8s folder"
+          cat K8s/deployment.yaml || echo "No deployment.yaml"
     '''
   }
+}
 }
