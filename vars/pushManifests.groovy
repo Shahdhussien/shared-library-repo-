5cf3ce4debffa2e260aa55e92989d21762974289
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 06:30:12 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,37 +1,57 @@
+// // def call() {
+// //     return {
+// //         stage('Push Manifests') {
+// //             sshagent(['github']) {
+// //                 sh '''
+// //                     echo "hhhhhhhhhhh"
+// //                     git config user.name "Jenkins"
+// //                     git config user.email "jenkins@example.com"
+// //                     git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+// //                     git checkout main
+// //                     git add .
+// //                     git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+// //                     git push origin main
+// //                 '''
+// //             }
+// //         }
+// //     }
+// // }
+
 // def call() {
-//     return {
-//         stage('Push Manifests') {
-//             sshagent(['github']) {
-//                 sh '''
-//                     echo "hhhhhhhhhhh"
-//                     git config user.name "Jenkins"
-//                     git config user.email "jenkins@example.com"
-//                     git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
-//                     git checkout main
-//                     git add .
-//                     git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
-//                     git push origin main
-//                 '''
-//             }
-//         }
+//     stage('Push Manifests') {
+//         withCredentials([usernamePassword(credentialsId: 'github-creds')]) {
+//     sh '''
+//         eval $(ssh-agent)
+//         ssh-add $SSH_KEY
+//         git config user.name "Jenkins"
+//         git config user.email "jenkins@example.com"
+//         git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+//         git checkout main
+//         git add .
+//         git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+//         git push origin main
+//     '''
+// }
+    
 //     }
 // }
 
 def call() {
     stage('Push Manifests') {
-        withCredentials([sshUserPrivateKey(credentialsId: 'github-creds', keyFileVariable: 'SSH_KEY')]) {
-    sh '''
-        eval $(ssh-agent)
-        ssh-add $SSH_KEY
-        git config user.name "Jenkins"
-        git config user.email "jenkins@example.com"
-        git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
-        git checkout main
-        git add .
-        git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
-        git push origin main
-    '''
-}
-    
+        withCredentials([usernamePassword(
+            credentialsId: 'github-creds', 
+            usernameVariable: 'GIT_USER', 
+            passwordVariable: 'GIT_TOKEN'
+        )]) {
+            sh '''
+                git config user.name "Jenkins"
+                git config user.email "jenkins@example.com"
+                git remote set-url origin https://${GIT_USER}:${GIT_TOKEN}@github.com/Shahdhussien/shared-library-repo-.git
+                git checkout main
+                git add .
+                git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+                git push origin main
+            '''
+        }
     }
 }
