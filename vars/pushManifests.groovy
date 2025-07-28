194e3857b4c1983dae25c5c7d99dbf2fb76917a7
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 04:37:11 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,19 +1,19 @@
 def call() {
-    stage('Push Manifests') {
-        echo 'Pushing updated manifests to Git repo...'
+    steps.stage('Push Manifests') {
+        steps.echo 'Pushing updated manifests to Git repo...'
         steps.sshagent(['github']) {
-         steps.sh """
-            git config user.name "Jenkins"
-            git config user.email "jenkins@example.com"
-            git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
-            git checkout main
-            git add .
-            git commit -m "Update manifests from Jenkins pipeline"
-            git push origin main
-        """
+            steps.sh """
+                git config user.name "Jenkins"
+                git config user.email "jenkins@example.com"
+                git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+                git checkout main
+                git add .
+                git commit -m "Update manifests from Jenkins pipeline" || echo "No changes to commit"
+                git push origin main
+            """
         }
     }
-    }
+}
 
 // def call() {
 //     steps.sshagent(['github']) {
