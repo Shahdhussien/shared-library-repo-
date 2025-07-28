1c2e4c5b3a0ef4eb9399cffb9f028c371e63e62a
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 06:34:44 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -67,6 +67,9 @@ def call() {
                 git config user.name "Jenkins"
                 git config user.email "jenkins@example.com"
                 git remote set-url origin https://${GIT_USER}:${GIT_TOKEN}@github.com/Shahdhussien/shared-library-repo-.git
+
+                git pull --rebase origin main  # ⬅️ أضف ده قبل push
+
                 git add .
                 git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
                 git push origin main
@@ -74,3 +77,4 @@ def call() {
         }
     }
 }
+
