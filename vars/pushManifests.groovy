44ba1692bc27873d0d46662550e6d5c21830ae8a
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 04:33:18 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,16 +1,17 @@
 def call() {
     stage('Push Manifests') {
         echo 'Pushing updated manifests to Git repo...'
-        withCredentials([usernamePassword(credentialsId: 'github', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN')]) {
+        sshagent(['github']){
         sh """
             git config user.name "Jenkins"
             git config user.email "jenkins@example.com"
-            git remote set-url origin https:"$GIT_USER":"$GIT_TOKEN"@github.com/Shahdhussien/shared-library-repo-.git
+            git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
             git checkout main
             git add .
             git commit -m "Update manifests from Jenkins pipeline"
             git push origin main
         """
+        }
     }
     }
-}
+
