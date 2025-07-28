f5b6a7941dc28d3d223a822eb0410d4bb08ca815
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 07:31:51 +0300

    first commit

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -3,6 +3,7 @@ def call() {
         echo ' Scanning Docker image...'
         withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
+        pwd
         trivy image "$DOCKER_USER"/myimg:"$BUILD_NUMBER"
         """
         }
