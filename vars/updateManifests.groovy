4a0cfbccf6bacdf560ee5641d00ef32bfbafd239
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 04:21:13 +0300

    first commit

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -2,7 +2,7 @@ def call() {
     stage('Update Manifests') {
         echo '📝 Updating Kubernetes manifests (YAML files) using sed...'
         sh '''
-        find K8s/ -name '*.yaml' -exec sed -i 's|image: my-app:.*|image: my-app:latest|' {} +
+        find K8s/ -name '*.yaml' -exec sed -i 's|image: flask-final-app:.*|image: myimg:latest|' {} +
         '''
     }
 }
