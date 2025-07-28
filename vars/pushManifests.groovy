2b5b6577607e2e99746732d04f71bf693113b522
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 06:14:29 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -19,18 +19,31 @@
 
 def call() {
     stage('Push Manifests') {
-        sshagent(['github']) {
-            sh '''
-             echo "hhhhhhhhhhh"
-                echo "🚀 Committing updated manifests..."
-                git config user.name "Jenkins"
-                git config user.email "jenkins@example.com"
-                git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
-                git checkout main
-                git add .
-                git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
-                git push origin main
-            '''
-        }
+        withCredentials([sshUserPrivateKey(credentialsId: 'github', keyFileVariable: 'SSH_KEY')]) {
+    sh '''
+        eval $(ssh-agent)
+        ssh-add $SSH_KEY
+        git config user.name "Jenkins"
+        git config user.email "jenkins@example.com"
+        git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+        git checkout main
+        git add .
+        git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+        git push origin main
+    '''
+}
+        // sshagent(['github']) {
+        //     sh '''
+        //      echo "hhhhhhhhhhh"
+        //         echo "🚀 Committing updated manifests..."
+        //         git config user.name "Jenkins"
+        //         git config user.email "jenkins@example.com"
+        //         git remote set-url origin git@github.com:Shahdhussien/shared-library-repo-.git
+        //         git checkout main
+        //         git add .
+        //         git commit -m "Update manifests from Jenkins pipeline" || echo "No changes"
+        //         git push origin main
+        //     '''
+        // }
     }
 }
