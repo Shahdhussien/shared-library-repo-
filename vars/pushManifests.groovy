8692131664c04c3a28dfaf797a99ea4cb914ad93
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 04:25:09 +0300

    first commit

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -4,6 +4,7 @@ def call() {
         sh '''
             git config user.name "Jenkins"
             git config user.email "jenkins@example.com"
+            git checkout main
             git add .
             git commit -m "Update manifests from Jenkins pipeline"
             git push origin main
