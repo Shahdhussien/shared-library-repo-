2d06e934a146e02152e8480efd22035dea7bc0d7
Shahdhussien <105828741+Shahdhussien@users.noreply.github.com>
2025-07-28 05:58:41 +0300

    first commit

--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,9 +1,30 @@
+// def call() {
+//     stage('Update Manifests') {
+//         echo '📝 Updating Kubernetes manifests (YAML files) using sed...'
+//         sh '''
+//         cd $WORKSPACE
+//                     sed -i 's|image:.*|image: shahd0/myimg:latest|' K8s/deployment.yaml
+//         '''
+// }
+//      stage('DEBUG: Check Image Line After sed') {
+//      steps {
+//      sh "grep 'image:' K8s/deployment.yaml"
+//   }
+// }
+   
+//     }
+
 def call() {
     stage('Update Manifests') {
         echo '📝 Updating Kubernetes manifests (YAML files) using sed...'
         sh '''
-        cd $WORKSPACE
-                    sed -i 's|image:.*|image: shahd0/myimg:latest|' K8s/deployment.yaml
+            cd $WORKSPACE
+            sed -i 's|image:.*|image: shahd0/myimg:latest|' K8s/deployment.yaml
         '''
+    }
+
+    stage('DEBUG: Check Image Line After sed') {
+        echo '🔍 Checking updated image line...'
+        sh "grep 'image:' K8s/deployment.yaml || echo 'No image line found!'"
     }
 }
